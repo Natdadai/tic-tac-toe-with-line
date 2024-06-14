23a7f0b8c1cd402f5eb904873696f8480fb6d8e3
Natdanai <natdadai.t@ku.th>
2024-06-14 14:34:01 +0700

    initialize

---
+++ src/main/java/com/application/tictactoe/TictactoeApplication.java
@@ -0,0 +1,13 @@
+package com.application.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeApplication.class, args);
+	}
+
+}
