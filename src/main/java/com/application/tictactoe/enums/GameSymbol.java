60185f85b8c567bff8afe367ee1cab7b374ffe80
Natdanai <natdadai.t@ku.th>
2024-06-17 02:13:26 +0700

    update GameSymbol

--- src/main/java/com/application/tictactoe/enums/GameSymbol.java
+++ src/main/java/com/application/tictactoe/enums/GameSymbol.java
@@ -1,5 +1,24 @@
 package com.application.tictactoe.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum GameSymbol {
-    X, O
+    X("X"), O("O");
+
+    private final String symbol;
+
+    public static GameSymbol getPlayerSymbol() {
+        return GameSymbol.X;
+    }
+
+    public static GameSymbol getComputerSymbol() {
+        return GameSymbol.O;
+    }
+
+    public static GameResult getGameResult(GameSymbol gameSymbol) {
+        return gameSymbol == getPlayerSymbol() ? GameResult.PLAYER : GameResult.COMPUTER;
+    }
 }
